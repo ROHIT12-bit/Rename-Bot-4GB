df1a02cfaebc301b518f58d0c908e81407a17d40
ROHIT12-bit <reddyrohit823@gmail.com>
2025-06-11 20:05:35 +0530

    Finally bot ready

--- config.py
+++ config.py
@@ -3,28 +3,28 @@ import os
 
 
 # Required Variables Config
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-ADMIN = int(os.environ.get("ADMIN", ""))
+API_ID = int(os.environ.get("API_ID", "20366634"))
+API_HASH = os.environ.get("API_HASH", "72095ec36984aa9ceb0dbaa9cec31559")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8130061359:AAH7On0zMtM6K_0W0vutFsLqWpFupFz7SUA")
+ADMIN = int(os.environ.get("ADMIN", "7845335174"))
 
 
 # Premium 4GB Renaming Client Config
-STRING_SESSION = os.environ.get("STRING_SESSION", "")
+STRING_SESSION = os.environ.get("STRING_SESSION", "BQE2xSoAZ4xAZqu6zc6eChA6FcA4YL6wuy46H0uCxB3HkqmvNpp4xWxiO0ZGpq-EuGw7GLw3BxciP2bM29UdE8TgE4zLmYDxTqUaWnijn00UqPFMi4yRw9d40C9vsaAbSft9sazZ_j2XxeHoY1_oBwVtO6JUy1IEU3XlZFVP5HVE7g0mhSVPd1M_PwtB1TVAXzlWCRR4ABz9WQY1N00wLrdRxB_7Tn1eTY7b6Ge8qOH-0fcI3N2_CgRnVmp21p55Gw8bqPQfAevU-pPTbnzaIRTR4g2T2qyi1WZjxFzokgQec-KnmeFFyRQfk-935sanBTM-_XOLOdQN4oW1RAIwyOzGRfw2PwAAAAHjDZXRAQ")
 
 
 # Log & Force Channel Config
-FORCE_SUBS = os.environ.get("FORCE_SUBS", "")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+FORCE_SUBS = os.environ.get("FORCE_SUBS", "-1002291875883")
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002611038436"))
 
 
 # Mongo DB Database Config
-DATABASE_URL = os.environ.get("DATABASE_URL", "")
+DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://rohitreddyathuru:R6Co7MOjTYQOAqcq@cluster0.xrwjpl9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = os.environ.get("DATABASE_NAME", "madflixbotz")
 
 
 # Other Variables Config
-START_PIC = os.environ.get("START_PIC", "https://graph.org/file/ad48ac09b1e6f30d2dae4.jpg")
+START_PIC = os.environ.get("START_PIC", "https://ibb.co/pvL1RH0c")
 
 
 
